a019363ad1aea8ffad539354f921345cf22ebc5e
sylwek84 <sylwek.wielun@gmail.com>
2017-10-20 11:55:49 +0200

    kontrolne

--- program2.c
+++ program2.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 
-int mnoz(int a, int b)
-{	return a * b;  }
-
-int dodaj(int a, int b)
-{	return a + b;  }
-
+int mnoz(int a, int b) { return a * b;  }
+int dodaj(int a, int b)  { return a + b;  }
 
 int main(void) /* oznacza ze nie mamy zadnych parametrow */
 	       /* mozemy zostawic puste w nawiasach lub void */
 {
 	int a = 2, b = 5;  /* deklaracj wraz z inicjalizacja */
-	char wybor = '*';
+	char wybor;
+
+	scanf("%c", &wybor);  /* czytaj z konsoli i zapisz pod wybor */
+
 		/*if(wyrazenie) {blok jesli wyrazenie prawdziwe}
 		else {blok jesli wyrazenie falszywe};	*/
 
 	if(wybor == '*') /* mozemy zmienic na dodawania + */
 			/* sprawdzenie czy rowne, to nie jest '=' */
- {
+{
 		printf("Wynik mnozenia: %i\n", mnoz(a, b));
 	}else{
 		printf("Wynik dodawania: %i\n", dodaj(a, b));
